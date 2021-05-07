6e6d032b4dd6267e28244a14646c6f54d0f4ab78
ThatoSeluku <Thato1000seluku@gmail.com>
2021-05-07 17:36:02 +0200

    Created string manipulator using python3, deleted unnecessary files

---
+++ stringmanipulator.py
@@ -0,0 +1,22 @@
+#StringManipulator program by Thato Seluku
+#Goal allow string input and output 
+
+user = "Thato Seluku"
+#1. Output instructions
+
+print("Welcome to String Manipulator 1.0, follow the instructions for a totally underwhelming outcome ");
+#2. Input data
+
+
+
+#Inputs:
+adjective = input("Enter your adjective: ")
+verb1 = input("Enter your verb: ")
+verb2 = input("Enter your verb: ")
+famous_person = input("Enter famous person associated with Kanye: ")
+
+
+#3. Output result
+StringManipulator = f"Fighting dogs feels so {adjective}! It makes me feel so happy because I love to {verb1}, please wear your mask and {verb2} just like Kanye and {famous_person}"
+
+print(StringManipulator)
